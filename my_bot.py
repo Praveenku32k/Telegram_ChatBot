a7a51ac4e1726932178d01855b1c7448091f1ee2
Praveen Kumar <praveenku32k@gmail.com>
2024-04-20 17:47:42 +0530

    update help function

--- my_bot.py
+++ my_bot.py
@@ -21,3 +21,56 @@ MODEL_NAME="gpt-3.5-turbo"
 bot =Bot(token=TOKEN)
 dispatcher =Dispatcher(bot)
 
+class Reference:
+    def __init__(self)->None:
+        self.resource = ""
+
+
+
+Reference = Reference()
+
+def clear_paste():
+    Reference.response = ""
+
+
+
+
+@dispatcher.message_handler(commands=['start'])
+async def command_start_handler(message : types.Message):
+
+    """This is the handler receives message with '/start' or '/help' command 
+
+    Args:
+     message (types.Message): _description_
+
+    """
+    await message.reply("Hi!\n I am a chat bot! created by Praveen\n How can I assist you !")
+
+
+
+@dispatcher.message_handler(commands=['help'])
+async def helper(message : types.Message):
+
+    """
+    A Handler to display help information    
+    """
+
+ 
+    help_command ="""
+    Hi There , I'm  a chat bot created by Praveen! Please follow this commands
+    /start - To start the chatbot
+    /clear - To clear the past conversation and context.
+    /help - To display this message.
+    I hope this helps. :)
+    """
+
+    await message.reply(help_command)
+
+
+
+
+if __name__ == "__main__":
+    executor.start_polling(dispatcher, skip_updates=True)
+
+
+
